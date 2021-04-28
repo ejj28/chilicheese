47a40e644d77afa7cce3cc3bc0885123c2510341
ejj28 <eriksinventions11@gmail.com>
2021-04-28 15:20:36 -0400

    Popup functionality

--- site/js/index.js
+++ site/js/index.js
@@ -20,3 +20,50 @@ var map = new ol.Map({
     zoom: 5
   })
 });
+
+var storePopup = new ol.Overlay({
+  element: document.getElementById('storePopup'),
+});
+map.addOverlay(storePopup);
+
+var popupElement = storePopup.getElement();
+
+
+
+map.on('click', function (evt) {
+  var feature = map.forEachFeatureAtPixel(evt.pixel,
+    function (feature, layer) {
+      return feature;
+    });
+  if (feature) {
+    console.log("hello")
+    var geometry = feature.getGeometry();
+    var coord = geometry.getCoordinates();
+
+
+    var hdms = ol.coordinate.toStringHDMS(ol.proj.toLonLat(coord));
+
+    console.log(feature.getProperties());
+    console.log(hdms);
+
+    $(popupElement).popover('dispose');
+    storePopup.setPosition(coord);
+    $(popupElement).popover({
+      container: popupElement,
+      placement: 'top',
+      trigger: 'manual',
+      animation: false,
+      html: true,
+      title: 'Taco Bell #' + feature.getProperties().store + ' <i class="fas fa-times popupClose"></i>',
+      content: '<p>' + feature.getProperties().address + '</p>',
+    });
+
+    $(popupElement).popover('show');
+    $(popupElement).find("h3").addClass("d-flex justify-content-between align-items-center")
+  }
+});
+
+
+$(document).on('click','.popupClose',function(){
+  $(popupElement).popover('dispose');
+});
