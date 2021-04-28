1c7b568d924621115514c3128877ba89f177fecc
ejj28 <eriksinventions11@gmail.com>
2021-04-28 12:44:08 -0400

    Basic location mapping

--- site/js/index.js
+++ site/js/index.js
@@ -1,12 +1,22 @@
+var ccbLocations = new ol.source.Vector({
+  url: 'data/ccb_locations_geoJSON.json',
+  format: new ol.format.GeoJSON()
+});
+
+var vectorLayer = new ol.layer.Vector({
+  source: ccbLocations
+});
+
 var map = new ol.Map({
-    target: 'map',
-    layers: [
-      new ol.layer.Tile({
-        source: new ol.source.OSM()
-      })
-    ],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([-96.46, 34.58]),
-      zoom: 5
-    })
-  });
+  target: 'map',
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.OSM()
+    }),
+    vectorLayer
+  ],
+  view: new ol.View({
+    center: ol.proj.fromLonLat([-96.46, 34.58]),
+    zoom: 5
+  })
+});
