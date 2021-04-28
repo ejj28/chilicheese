4f4f0dc8d7d7c6d84395bd452be7a28b78a44a60
ejj28 <eriksinventions11@gmail.com>
2021-04-27 21:20:54 -0400

    Remove unneeded script

--- scripts/coords2address.py
+++
@@ -1,7 +0,0 @@
-# Takes a csv of coordinates and uses the Mapbox Geocoding API to get the addresses
-# Usage: python coords2address.py input.csv output.csv
-
-import sys
-
-inputPath = sys.argv[1]
-outputPath = sys.argv[2]
